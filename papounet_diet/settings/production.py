117d2c9c28eb8e8255a4c3f8588dc709d717f386
Fabrice-64 <fab.jaouen@gmail.com>
2021-01-14 15:47:45 +0100

    Adapt the settings to Digital Ocean

--- papounet_diet/settings/production.py
+++ papounet_diet/settings/production.py
@@ -1,10 +1,9 @@
 import dj_database_url
-
 from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["papounet-diet-h727t.ondigitalocean.app"]
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 MIDDLEWARE = [
@@ -19,15 +18,4 @@ MIDDLEWARE = [
 ]
 
 # Requested for deployment with Heroku, when using PostgreSQL
-DATABASES['default'] = dj_database_url.config(conn_max_age=600,
-                                              ssl_require=True)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
-    },
-}
+DATABASES['default'] = dj_database_url.parse(os.environ.get('DATABASE_URL'))
