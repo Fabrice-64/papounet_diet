872c0c808b94921a027bff9df483b75821115673
Fabrice-64 <fab.jaouen@gmail.com>
2021-01-14 21:09:06 +0100

    Add Database pwd, user, name in papounet_diet.settings.production

--- papounet_diet/settings/production.py
+++ papounet_diet/settings/production.py
@@ -10,10 +10,12 @@ ALLOWED_HOSTS = ['159.89.0.27']
 
 DATABASES = {
     'default': {
-        'ENGINE' : 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'papounet_diet',
+        'USER': 'papounet_diet_user',
+        'PASSWORD': '@HaRiBo2021',
+        'HOST': 'localhost',
+        'PORT': '',
 
     }
 }
