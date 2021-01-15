ca31f38842bbe9d2e68fcd61567fcce21f4977d1
Fabrice-64 <fab.jaouen@gmail.com>
2021-01-15 14:08:54 +0100

    Production settings : set DEBUG = False (Errors static files and server 500 solved)

--- papounet_diet/settings/production.py
+++ papounet_diet/settings/production.py
@@ -1,6 +1,6 @@
 from .base import *
 from django.core.management.utils import get_random_secret_key
-DEBUG = True
+DEBUG = False
 SECRET_KEY = get_random_secret_key()
 ADMINS = {
     ('Fabrice J', 'fabricejaouen@yahoo.com'),
