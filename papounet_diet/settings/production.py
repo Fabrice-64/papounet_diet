46884db060b531adcddc3b96c822e9a329b133af
Fabrice-64 <fab.jaouen@gmail.com>
2021-01-17 11:29:17 +0100

    Correct secret key settings to get back to a canonical approach

--- papounet_diet/settings/production.py
+++ papounet_diet/settings/production.py
@@ -1,8 +1,8 @@
 from .base import *
-from django.core.management.utils import get_random_secret_key
 
-DEBUG = True
-SECRET_KEY = get_random_secret_key()
+
+DEBUG = False
+
 ADMINS = {
     ('Fabrice J', 'fabricejaouen@yahoo.com'),
 }
