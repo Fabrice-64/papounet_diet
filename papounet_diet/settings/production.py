553b31439e21a394629470643e41fe21abf1cb9e
Fabrice-64 <fab.jaouen@gmail.com>
2021-01-17 17:02:21 +0100

    Add Sentry. Tested: OK Create environment variable for the debug message to Sentry

--- papounet_diet/settings/production.py
+++ papounet_diet/settings/production.py
@@ -1,6 +1,17 @@
-from .base import *
 import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+from .base import *
 
+
+sentry_sdk.init(
+    dsn=os.getenv('SENTRY_DSN'),
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 DEBUG = False
 
--- papounet_diet/urls.py
+++ papounet_diet/urls.py
@@ -17,10 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from customer import views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('food_items/', include('food_items.urls')),
     path('customer/', include('customer.urls')),
     path('', views.home, name="home"),
-    path('home/', views.home, name="home")]
+    path('home/', views.home, name="home")
+    ]
