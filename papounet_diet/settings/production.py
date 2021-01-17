017cf819a20b336aa629cf934e636dc1b2422482
Fabrice-64 <fab.jaouen@gmail.com>
2021-01-17 14:54:24 +0100

    Add environment variables for Travis

--- papounet_diet/settings/production.py
+++ papounet_diet/settings/production.py
@@ -12,9 +12,9 @@ ALLOWED_HOSTS = ['206.189.55.69', 'localhost']
 DATABASES = {
     'default': {
         'ENGINE' : 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'papounet_diet_db',
-        'USER': 'fabrice',
-        'PASSWORD': '@HaRiBo2021',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '',
 
